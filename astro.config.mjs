c6bc438b40b873953ec1a72a4f5b1ad7e9c9ac2e
AnthonyLzq <sluzquinosa@uni.pe>
2023-02-02 23:52:45 -0500

    feat: implemented blog preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config'
+import { remarkReadingTime } from './src/plugins/remarkReadingTime.mjs'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    extendDefaultPlugins: true
+  }
+})
---
+++ src/utils/tech.ts
@@ -0,0 +1,82 @@
+export type Tech = {
+  name: string
+  image: string
+  url: string
+}
+
+const tech: Record<string, Tech> = {
+  typescript: {
+    name: 'TypeScript',
+    image:
+      'https://profilinator.rishav.dev/skills-assets/typescript-original.svg',
+    url: 'https://www.typescriptlang.org'
+  },
+  javascript: {
+    name: 'JavaScript',
+    image:
+      'https://profilinator.rishav.dev/skills-assets/javascript-original.svg',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+  },
+  node: {
+    name: 'Node.js',
+    image:
+      'https://profilinator.rishav.dev/skills-assets/nodejs-original-wordmark.svg',
+    url: 'https://nodejs.org'
+  },
+  express: {
+    name: 'Express',
+    image:
+      'https://profilinator.rishav.dev/skills-assets/express-original-wordmark.svg',
+    url: 'https://expressjs.com'
+  },
+  fastify: {
+    name: 'Fastify',
+    image: 'https://avatars.githubusercontent.com/u/24939410',
+    url: 'https://fastify.io'
+  },
+  graphql: {
+    name: 'GraphQL',
+    image: 'https://profilinator.rishav.dev/skills-assets/graphql.png',
+    url: 'https://graphql.org'
+  },
+  mongo: {
+    name: 'MongoDB',
+    image:
+      'https://profilinator.rishav.dev/skills-assets/mongodb-original-wordmark.svg',
+    url: 'https://www.mongodb.com'
+  },
+  postgres: {
+    name: 'PostgreSQL',
+    image:
+      'https://profilinator.rishav.dev/skills-assets/postgresql-original-wordmark.svg',
+    url: 'https://www.postgresql.org'
+  },
+  remix: {
+    name: 'Remix',
+    image: 'https://avatars.githubusercontent.com/u/64235328?s=200&v=4',
+    url: 'https://remix.run'
+  },
+  material: {
+    name: 'MaterialUI',
+    image: 'https://profilinator.rishav.dev/skills-assets/mui.png',
+    url: 'https://material-ui.com'
+  },
+  supabase: {
+    name: 'Supabase',
+    image: 'https://avatars.githubusercontent.com/u/54469796',
+    url: 'https://supabase.io'
+  },
+  prisma: {
+    name: 'Prisma',
+    image: 'https://profilinator.rishav.dev/skills-assets/prisma.png',
+    url: 'https://www.prisma.io'
+  },
+  react: {
+    name: 'React',
+    image:
+      'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg',
+    url: 'https://reactjs.org'
+  }
+}
+
+export { tech }
