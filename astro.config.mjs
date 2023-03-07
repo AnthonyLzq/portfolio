57b663535a1e1c9e74158f6bf2d96fcdfd015cc7
AnthonyLzq <sluzquinosa@uni.pe>
2023-03-06 21:15:23 -0500

    feat: implemented sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { remarkReadingTime } from './src/plugins/remarkReadingTime.mjs'
 import react from '@astrojs/react'
 
 // https://astro.build/config
+import sitemap from '@astrojs/sitemap'
+
+// https://astro.build/config
 export default defineConfig({
+  site: 'https://anthonylzq.dev',
   markdown: {
     remarkPlugins: [remarkReadingTime],
     extendDefaultPlugins: true
   },
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 })
